36401d8d5377b9383e392df08369ebd9afb91ad9
Luc Randrianomenjanahary <luchariman@gmail.com>
2020-12-01 23:06:36 +0100

    Removing unused controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConsoleModule } from 'nestjs-console';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DataImportModule } from './data-import/data-import.module';
 import { RegionsModule } from './regions/regions.module';
@@ -13,7 +12,6 @@ import { RegionsModule } from './regions/regions.module';
     DataImportModule,
     RegionsModule,
   ],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
