bb60f08542021f16ca6ff904352cf4e405e502b7
Luc Randrianomenjanahary <luchariman@gmail.com>
2020-12-03 20:04:22 +0100

    Paginating regions query result

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as path from 'path';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe())
   const packageJson = require(path.join(__dirname, '..', 'package.json'));
   const options = new DocumentBuilder()
     .setTitle('COVID-19 API')
--- src/regions/regions.controller.ts
+++ src/regions/regions.controller.ts
@@ -1,8 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ApiOkResponse, ApiProperty, ApiPropertyOptional, ApiTags } from '@nestjs/swagger';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Type } from 'class-transformer';
+import { IsNumber, IsOptional, Min, ValidateNested } from 'class-validator';
 import { Repository } from 'typeorm';
 import { Region } from './region.entity';
+
+class ListQuery {
+  @ApiPropertyOptional()
+  @IsNumber()
+  @Type(() => Number)
+  @Min(1)
+  @IsOptional()
+  page?: number;
+
+  @ApiPropertyOptional()
+  @IsNumber()
+  @Type(() => Number)
+  @Min(1)
+  @IsOptional()
+  itemsPerPage?: number;
+}
+
+class ListResult {
+  @ApiProperty()
+  page: number;
+
+  @ApiProperty()
+  itemsPerPage: number;
+
+  @ApiProperty({ type: [Region] })
+  entries: Region[];
+}
 
 @ApiTags('Regions')
 @Controller('regions')
@@ -13,9 +42,11 @@ export class RegionsController {
   ) {}
 
   @Get()
-  @ApiOkResponse({ type: Region, isArray: true })
-  get() {
-    // TODO: Add filter parameters
-    return this.regionsRepository.find();
+  @ApiOkResponse({ type: ListResult })
+  async get(@Query() { page, itemsPerPage }: ListQuery): Promise<ListResult> {
+    page = page || 1;
+    itemsPerPage = itemsPerPage || 30;
+    const entries = await this.regionsRepository.find({ skip: (page - 1) * itemsPerPage, take: itemsPerPage });
+    return { page, itemsPerPage, entries };
   }
 }
